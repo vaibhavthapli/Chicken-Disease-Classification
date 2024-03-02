319969cb4f28535f4d3712e394cbda943e3fd31f
Vaibhav Singh Thapli <vaibhavthapli123@gmail.com>
2024-03-02 16:25:56 +0530

    evaluation added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 from cnnClassifier.components import *
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 from cnnClassifier.utils.common import read_yaml, create_directories
 import os
-
 from cnnClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                EvaluationConfig, 
+                                                PrepareBaseModelConfig, 
+                                                PrepareCallbacksConfig, 
+                                                TrainingConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -91,3 +93,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
